7125a81ccb8b200645ef96cd549d902cc89b81cb
Roy Anger <roy@royanger.com>
2022-04-20 01:41:06 -0400

    Create API route to return random tips.
    You can configure the route by editing tips.js and changing the 'const numberOfTips = 4;' line

--- api/index.js
+++ api/index.js
@@ -1,26 +1,31 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 // load vars from ENV variables
-dotenv.config()
-const port = process.env.PORT ? process.env.PORT : 3002
-const url = process.env.URL ? process.env.URL : 'localhost'
+dotenv.config();
+const port = process.env.PORT ? process.env.PORT : 3002;
+const url = process.env.URL ? process.env.URL : 'localhost';
+
+// import routes
+import tipsRoute from './routes/tips.js';
 
 // initialize express server
-const app = express()
+const app = express();
 app.use(
   cors({
     origin: process.env.APP_URL,
     credentials: true,
   })
-)
+);
 
 app.get('/', (req, res) => {
-  res.send('API for Wellness Rips')
-})
+  res.send('API for Wellness Tips');
+});
+
+app.get('/api/tips', tipsRoute);
 
 // start the server
 app.listen(port, url, () => {
-  console.log(`EXPRESS: started on http://${url}:${port}`)
-})
+  console.log(`EXPRESS: started on http://${url}:${port}`);
+});
